b319d39fa3ad90e753c6f0561af116d6918dd848
Maciej M <mcj.mezyk@gmail.com>
2024-04-08 14:12:53 +0200

    Refactor PDFUnlocker to use OOP and add Tkinter GUI

--- main.py
+++ main.py
@@ -1,27 +1,71 @@
 #! python3
 # PDFUnlocker - a simple script to remove passwords from your PDF files (assuming you know the password).
-import PyPDF2
+import tkinter as tk
+from tkinter import filedialog, simpledialog, messagebox
 import os
+import PyPDF2
 
-# Enter password to your pdf files here
-PASSWORD = ''
 
-input_folder = 'in'
-output_folder = 'out'
+class PDFUnlocker:
+    def __init__(self):
+        self.root = tk.Tk()
+        self.root.title("PDF Unlocker")
+
+        self.input_folder = ""
+        self.output_folder = ""
+        self.password = ""
+
+        tk.Button(self.root, text="Choose input directory", command=self.choose_input_folder).pack()
+        tk.Button(self.root, text="Choose output directory", command=self.choose_output_folder).pack()
+        self.password_entry = tk.Entry(self.root)
+        self.password_entry.pack()
+        self.password_entry.insert(0, "5836294137")
+        tk.Button(self.root, text="Unlock PDFs", command=self.unlock_pdfs).pack()
+
+        tk.Button(self.root, text="Exit", command=self.root.destroy).pack()
+
+    def choose_input_folder(self):
+        self.input_folder = filedialog.askdirectory(title="Choose input directory")
+        print(f"Input directory: {self.input_folder}")
+
+    def choose_output_folder(self):
+        self.output_folder = filedialog.askdirectory(title="Choose output directory")
+        print(f"Output dir: {self.output_folder}")
+
+    def enter_password(self):
+        self.password = simpledialog.askstring("Password", "Enter password: ", initialvalue="5836294137")
+
+    def unlock_pdfs(self):
+        if not self.input_folder:
+            messagebox.showerror("Error", "Please choose input_folder!")
+
+        elif not self.output_folder:
+            messagebox.showerror("Error", "Please choose input_folder!")
+
+        else:
+            password = self.password_entry.get()
+            for filename in os.listdir(self.input_folder):
+                if filename.endswith('.pdf'):
+                    input_path = os.path.join(self.input_folder, filename)
+                    output_path = os.path.join(self.output_folder, filename.replace('.pdf', '_unlocked.pdf'))
+                    with open(input_path, 'rb') as lockedPDF:
+                        reader = PyPDF2.PdfReader(lockedPDF)
+                        if reader.is_encrypted:
+                            try:
+                                reader.decrypt(password)
+                            except Exception as e:
+                                messagebox.showerror("Error", f"Failed to decrypt {filename}. Error: {e}")
+                                continue
+                        with open(output_path, 'wb') as unlockedPDF:
+                            writer = PyPDF2.PdfWriter()
+                            for i in range(len(reader.pages)):
+                                writer.add_page(reader.pages[i])
+                            writer.write(unlockedPDF)
+                    print(f"{filename} unlocked successfully is now in {output_path}")
+
+    def run(self):
+        self.root.mainloop()
+
 
 if __name__ == '__main__':
-    print(os.getcwd())
-    for filename in os.listdir(input_folder):
-        if filename.endswith('.pdf'):
-            input_path = os.path.join(input_folder, filename)
-            output_path = os.path.join(output_folder, filename.replace('.pdf', '_unlocked.pdf'))
-            with open(input_path, 'rb') as lockedPDF:
-                reader = PyPDF2.PdfReader(lockedPDF)
-                reader.decrypt(PASSWORD)
-
-                with open(output_path, 'wb') as unlockedPDF:
-                    writer = PyPDF2.PdfWriter()
-                    for i in range(len(reader.pages)):
-                        writer.add_page(reader.pages[i])
-                    writer.write(unlockedPDF)
-            print(f"{filename} unlocked successfully is now in {output_path}")
+    pdf_unlocker = PDFUnlocker().run()
